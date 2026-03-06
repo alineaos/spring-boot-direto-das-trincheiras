e66cfd154fe8e6e7a1cdab4b749559f8eb84ae8c
Aline <alineoliveiradesantana2013@gmail.com>
2026-03-06 13:12:13 -0300

    Aula 18: Desacoplando domínio de requisições e respostas

--- anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
+++ anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
@@ -2,9 +2,13 @@ package academy.devdojo.controller;
 
 
 import academy.devdojo.domain.Producer;
+import academy.devdojo.request.ProducerPostRequest;
+import academy.devdojo.response.ProducerGetResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -23,6 +28,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @Slf4j
 public class ProducerController {
     private List<Producer> producers = new ArrayList<>(Producer.getProducers());
+
     @GetMapping()
     public List<Producer> listAll(@RequestParam(required = false) String name) {
         if (name == null) return producers;
@@ -42,11 +48,23 @@ public class ProducerController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             headers = "x-api-key")
-    public Producer save(@RequestBody Producer producer, @RequestHeader HttpHeaders headers){
+    public ResponseEntity<ProducerGetResponse> save(@RequestBody ProducerPostRequest producerPostRequest, @RequestHeader HttpHeaders headers) {
         log.info("{}", headers);
-        producer.setId(ThreadLocalRandom.current().nextLong(100_000)); //settando o id nesse momento, o que foi passado na requisição será ignorado
-        producers.add(producer);
-        return producer;
+        Producer producer = Producer.builder()
+                .id(ThreadLocalRandom.current().nextLong(100_000)) //settando o id nesse momento, o que foi passado na requisição será ignorado
+                .name(producerPostRequest.getName())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Producer.getProducers().add(producer);
+
+        ProducerGetResponse response = ProducerGetResponse.builder()
+                .id(producer.getId())
+                .name(producer.getName())
+                .createdAt(producer.getCreatedAt())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
 }
--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,16 +16,19 @@ public class Producer {
     private Long id;
     @JsonProperty("name") // Se o nome na api estiver diferente do que no sistema, essa anotação faz uma "conversão". Mais detalhes Notion
     private String name;
-    @Getter
+    private LocalDateTime createdAt;
     private static List<Producer> producers = new ArrayList<>();
 
     static {
-        Producer mappa = Producer.builder().id(1L).name("Mappa").build();
-        Producer toeiAnimation = Producer.builder().id(2L).name("Toei Animation").build();
-        Producer madhouse = Producer.builder().id(3L).name("Madhouse").build();
-        Producer pierrotStudio = Producer.builder().id(4L).name("Studio Pierrot").build();
-        Producer witStudio = Producer.builder().id(5L).name("Wit Studio").build();
+        Producer mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
+        Producer toeiAnimation = Producer.builder().id(2L).name("Toei Animation").createdAt(LocalDateTime.now()).build();
+        Producer madhouse = Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build();
+        Producer pierrotStudio = Producer.builder().id(4L).name("Studio Pierrot").createdAt(LocalDateTime.now()).build();
+        Producer witStudio = Producer.builder().id(5L).name("Wit Studio").createdAt(LocalDateTime.now()).build();
         producers.addAll(List.of(mappa, toeiAnimation, madhouse, pierrotStudio, witStudio));
     }
 
+    public static List<Producer> getProducers() {
+        return producers;
+    }
 }
