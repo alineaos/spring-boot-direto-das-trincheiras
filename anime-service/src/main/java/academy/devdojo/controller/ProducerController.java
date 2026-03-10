d008cf0d7569655eb11519378b5a466ba39946fd
Aline <alineoliveiradesantana2013@gmail.com>
2026-03-10 12:59:18 -0300

    Aula 30: Injeção de Dependências e Spring Beans

--- anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
+++ anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
@@ -8,6 +8,7 @@ import academy.devdojo.request.ProducerPutRequest;
 import academy.devdojo.response.ProducerGetResponse;
 import academy.devdojo.response.ProducerPostResponse;
 import academy.devdojo.service.ProducerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -29,13 +30,10 @@ import java.util.List;
 @RestController
 @RequestMapping("v1/producers")
 @Slf4j
+@RequiredArgsConstructor
 public class ProducerController {
     private static final ProducerMapper MAPPER = ProducerMapper.INSTANCE;
-    private ProducerService service;
-
-    private ProducerController(){
-        this.service = new ProducerService();
-    }
+    private final ProducerService service;
 
     @GetMapping()
     public ResponseEntity<List<ProducerGetResponse>> listAll(@RequestParam(required = false) String name) {
