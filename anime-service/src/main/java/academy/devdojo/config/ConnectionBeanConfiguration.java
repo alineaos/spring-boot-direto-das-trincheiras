8c1207f6567175bb4fee138c959f63390b68f717
Aline <alineoliveiradesantana2013@gmail.com>
2026-03-14 16:27:56 -0300

    Aula 53: @ConfigurationProperties

--- anime-service/src/main/java/academy/devdojo/config/ConnectionBeanConfiguration.java
+++ anime-service/src/main/java/academy/devdojo/config/ConnectionBeanConfiguration.java
@@ -1,23 +1,29 @@
 package academy.devdojo.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-public class ConnectionConfiguration {
-    @Value("${database.url}")
-    private String url;
-    @Value("${database.username}")
-    private String username;
-    @Value("${database.password}")
-    private String password;
+@RequiredArgsConstructor
+public class ConnectionBeanConfiguration {
+    private final ConnectionConfigurationProperties connectionProperties;
+
+//    @Value("${database.url}")
+//    private String url;
+//    @Value("${database.username}")
+//    private String username;
+//    @Value("${database.password}")
+//    private String password;
 
     @Bean
     //@Profile("mysql")
     public Connection connectionMySql() {
-        return new Connection(url, username, password);
+        return new Connection(connectionProperties.url(),
+                connectionProperties.username(),
+                connectionProperties.password());
     }
 
     //@Primary
