b2044939645b4ba934c7c0decf4a987ac50f4522
Aline <alineoliveiradesantana2013@gmail.com>
2026-03-31 17:04:15 -0300

    Aula 156: @ConfigurationProperties

--- user-service/src/main/java/academy/devdojo/UserServiceApplication.java
+++ user-service/src/main/java/academy/devdojo/UserServiceApplication.java
@@ -1,9 +1,14 @@
 package academy.devdojo;
 
+import academy.devdojo.config.BrasilApiConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+//@EnableConfigurationProperties(BrasilApiConfigurationProperties.class) // outra forma de ativar o ConfigurationProperties
+@ConfigurationPropertiesScan
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
---
+++ user-service/src/main/java/academy/devdojo/config/BrasilApiConfigurationProperties.java
@@ -0,0 +1,8 @@
+package academy.devdojo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+// Classe utilizada para usar as configurações no application.yml
+@ConfigurationProperties(prefix = "brasil-api")
+public record BrasilApiConfigurationProperties(String baseUrl, String cepUri) {
+}
