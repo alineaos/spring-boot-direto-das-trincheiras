aaa1e40be3c8d4850c819ca5bf305da5dddfb1c4
Aline <alineoliveiradesantana2013@gmail.com>
2026-04-01 15:45:31 -0300

    Aula 165: Migração de banco de dados com Flyway

---
+++ anime-service/src/main/resources/db/migration/V2__initialize_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO anime_service.anime (id, name) VALUES (1, 'Hunter X Hunter');
+INSERT INTO anime_service.anime (id, name) VALUES (2, 'Naruto');
+INSERT INTO anime_service.anime (id, name) VALUES (3, 'One Piece');
+INSERT INTO anime_service.anime (id, name) VALUES (4, 'Dragon Ball');
+INSERT INTO anime_service.anime (id, name) VALUES (5, 'Jujutsu Kaisen');
+INSERT INTO anime_service.anime (id, name) VALUES (6, 'Pokemon');
+INSERT INTO anime_service.anime (id, name) VALUES (7, 'Shingeki no Kyojin');
+INSERT INTO anime_service.anime (id, name) VALUES (8, 'Banana Fish');
